f7dbda7338a8be2f46ffeae4d139983b5bc81366
Vindra Ganzz <vindraganzz532@gmail.com>
2025-03-28 13:58:42 +0700

    Update maaf.py

--- Data/maaf.py
+++ Data/maaf.py
@@ -6,7 +6,4 @@ from rich import print as printf
 from rich.prompt import Prompt
 
 os.system('clear')
-printf(Panel(f"""Saya Minta Maaf🙏 Dengan script ini, Saya Akan memperbaikinya.
-hanya saja Membutuhkan Waktu Untuk Memperbaikinya untuk waktu perbaikan akan update besok
-mungkin besok akan berjalan dengan normal lagi bila tidak ada kendala error dari website scrapping mohon maaf ya kak🙏.\n update script besok pukul; 20:41 Sore""",width=57,style="bold black"))
-sys.exit(1)
+printf(Panel(f"""Saya Minta Maaf🙏 Dengan script ini, Saya Akan memperbaikinya. hanya saja Membutuhkan Waktu Untuk Memperbaikinya untuk waktu perbaikan akan update besok mungkin besok akan berjalan dengan normal lagi bila tidak ada kendala error dari website scrapping mohon maaf ya kak🙏.\n update script besok pukul; 20:41 Sore""",width=57,style="bold white"))
