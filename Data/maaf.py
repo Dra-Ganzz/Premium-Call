b577259db787c49eba37afa3ccf1197742cfc4f1
Vindra Ganzz <vindraganzz532@gmail.com>
2026-06-09 20:45:06 +0700

    Delete Data/maaf.py
    Signed-off-by: Vindra Ganzz <vindraganzz532@gmail.com>

--- Data/maaf.py
+++
@@ -1,9 +0,0 @@
-import os,sys,time
-from rich.panel import Panel
-from rich.columns import Columns
-from rich.console import Console
-from rich import print as printf
-from rich.prompt import Prompt
-
-os.system('clear')
-printf(Panel(f"""SSaya Minta Maaf🙏 Dengan script ini, Saya Akan memperbaikinya. hanya saja Membutuhkan Waktu Untuk Memperbaikinya untuk waktu perbaikan akan update besok mungkin besok akan berjalan dengan normal lagi bila tidak ada kendala error dari website scrapping mohon maaf ya kak🙏.\n update script besok pukul; 20:41 Sore""",width=57,style="bold white"))
